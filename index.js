c03e5832355b46aa3a76773fe83aac6bfe58b609
igor-stefanski <igor@nexcode.pl>
2023-03-30 09:19:20 +0200

    kolkokrzyzyk

---
+++ index.js
@@ -0,0 +1,5 @@
+const computerPlayer = document.getElementById("computer");
+const multiPlayer = document.getElementById("multi");
+const resetButton = document.getElementById("reset");
+const cross = document.getElementById("cross");
+const circle = document.getElementById("circle");
